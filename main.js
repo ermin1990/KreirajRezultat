99254f5f15d80ca573668915a4ac1fa4f26af3b0
ErminSelimovic <92504422+ErminSelimovic@users.noreply.github.com>
2022-04-25 07:22:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function autoClick() {
+    $("#download").click();
+}
+
+$(document).ready(function() {
+    var element = $("#htmlContent");
+
+    $("#download").on('click', function() {
+
+        html2canvas(element, {
+            onrendered: function(canvas) {
+                var imageData = canvas.toDataURL("image/jpg");
+                var newData = imageData.replace(/^data:image\/jpg/, "data:application/octet-stream");
+                $("#download").attr("download", "Rezultat.jpg").attr("href", newData);
+            }
+        });
+
+    });
+});
